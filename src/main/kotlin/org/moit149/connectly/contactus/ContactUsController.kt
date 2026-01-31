6e93b29a779e62a889b421a43919f3b469430de3
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-01-31 19:35:18 +0800

    feat: use response codes

--- src/main/kotlin/org/moit149/connectly/contactus/ContactUsController.kt
+++ src/main/kotlin/org/moit149/connectly/contactus/ContactUsController.kt
@@ -1,5 +1,7 @@
 ﻿package org.moit149.connectly.contactus
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,17 +12,17 @@ import java.time.LocalDateTime
 @RequestMapping("/api/contactus")
 class ContactUsController(private val contactRepository: ContactRepository) {
     @PostMapping
-    fun addContact(@RequestBody addContactDto: AddContactDto): AddContactResponse? {
+    fun addContact(@RequestBody addContactDto: AddContactDto): ResponseEntity<AddContactResponse>? {
         val contact = addContactDto.toContactModel()
 
         try {
             contactRepository.save(contact)
         }
         catch (e: Exception) {
-            return null
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null)
         }
 
-        return AddContactResponse(contact.receivedAt)
+        return ResponseEntity.ok(AddContactResponse())
     }
 }
 
