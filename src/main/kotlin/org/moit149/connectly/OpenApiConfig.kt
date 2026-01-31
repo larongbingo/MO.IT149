a638881e91aaef158506dbc4dbcce7d78e72e110
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-01-31 18:20:19 +0800

    feat: add google oauth in the swagger doc

--- src/main/kotlin/org/moit149/connectly/OpenApiConfig.kt
+++ src/main/kotlin/org/moit149/connectly/OpenApiConfig.kt
@@ -1,8 +1,37 @@
 ﻿package org.moit149.connectly
 
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.Scopes
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class OpenApiConfig {
-    //fun connectlyOpenApiConfig(): OpenApi = OpenApi()
+    @Bean
+    fun connectlyOpenApiConfig(): OpenAPI = OpenAPI()
+        .addSecurityItem(SecurityRequirement().addList("googleOauth"))
+        .components(
+            Components().addSecuritySchemes(
+                "googleOauth",
+                SecurityScheme().type(SecurityScheme.Type.OAUTH2)
+                    .flows(
+                        OAuthFlows().authorizationCode(
+                            OAuthFlow()
+                                .authorizationUrl("https://accounts.google.com/o/oauth2/v2/auth")
+                                .tokenUrl("https://oauth2.googleapis.com/token")
+                                .scopes(
+                                    Scopes()
+                                        .addString("openid", "OpenID")
+                                        .addString("profile", "Profile")
+                                        .addString("email", "Email")
+                                )
+                        )
+                    )
+            )
+        )
 }
