d2e5256c8552189f0289643d2bed4b6a6dcd2e0c
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-02-02 15:38:29 +0800

    feat: add auth0 api for fetching user profile

---
+++ src/main/kotlin/org/moit149/connectly/users/Auth0Service.kt
@@ -0,0 +1,31 @@
+﻿package org.moit149.connectly.users
+
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
+import java.time.LocalDateTime
+
+@Service
+class Auth0Service(val restBuilder: RestClient.Builder) {
+    private val restClient: RestClient by lazy { restBuilder.baseUrl("https://ewan.au.auth0.com/").build() }
+
+    fun getUserProfile(accessToken: String) =
+        restClient
+            .get()
+            .uri { it.path("/userinfo").build() }
+            .header("Authorization", "Bearer $accessToken")
+            .retrieve()
+            .body<Auth0UserProfile>()
+}
+
+data class Auth0UserProfile(
+    val sub: String,
+    val given_name: String,
+    val family_name: String,
+    val nickname: String,
+    val name: String,
+    val picture: String,
+    val updated_at: LocalDateTime,
+    val email: String,
+    val email_verified: Boolean
+)
